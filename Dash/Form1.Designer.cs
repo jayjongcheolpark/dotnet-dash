f44b426bb4a2aecfe6e130605d9e61bbf071fb73
Jay Park <jay.jongcheol.park@gmail.com>
2018-03-21 22:56:26 -0400

    initial commit

---
+++ Dash/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Dash
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this._pnHeader = new System.Windows.Forms.Panel();
+      this.button1 = new System.Windows.Forms.Button();
+      this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+      this._pnHeader.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // _pnHeader
+      // 
+      this._pnHeader.BackColor = System.Drawing.Color.DarkSlateGray;
+      this._pnHeader.Controls.Add(this.button1);
+      this._pnHeader.Dock = System.Windows.Forms.DockStyle.Top;
+      this._pnHeader.Location = new System.Drawing.Point(0, 0);
+      this._pnHeader.Name = "_pnHeader";
+      this._pnHeader.Size = new System.Drawing.Size(1045, 80);
+      this._pnHeader.TabIndex = 0;
+      // 
+      // button1
+      // 
+      this.button1.FlatAppearance.BorderSize = 0;
+      this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.button1.Font = new System.Drawing.Font("Consolas", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+      this.button1.Location = new System.Drawing.Point(991, 12);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(42, 36);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "X";
+      this.button1.UseVisualStyleBackColor = true;
+      // 
+      // Form1
+      // 
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+      this.ClientSize = new System.Drawing.Size(1045, 664);
+      this.Controls.Add(this._pnHeader);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+      this.Name = "Form1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this._pnHeader.ResumeLayout(false);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Panel _pnHeader;
+    private System.Windows.Forms.Button button1;
+    private System.ComponentModel.BackgroundWorker backgroundWorker1;
+  }
+}
+
---
+++ Dash/FormDashboard.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Dash
+{
+  partial class FormDashboard
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.panel2 = new System.Windows.Forms.Panel();
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.button3 = new System.Windows.Forms.Button();
+      this.button4 = new System.Windows.Forms.Button();
+      this.panel1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // panel1
+      // 
+      this.panel1.BackColor = System.Drawing.Color.DarkSlateGray;
+      this.panel1.Controls.Add(this.button4);
+      this.panel1.Controls.Add(this.button3);
+      this.panel1.Controls.Add(this.button2);
+      this.panel1.Controls.Add(this.button1);
+      this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+      this.panel1.Location = new System.Drawing.Point(0, 0);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(160, 647);
+      this.panel1.TabIndex = 0;
+      // 
+      // panel2
+      // 
+      this.panel2.BackColor = System.Drawing.Color.DarkSlateGray;
+      this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+      this.panel2.Location = new System.Drawing.Point(160, 0);
+      this.panel2.Name = "panel2";
+      this.panel2.Size = new System.Drawing.Size(890, 39);
+      this.panel2.TabIndex = 0;
+      // 
+      // button1
+      // 
+      this.button1.FlatAppearance.BorderSize = 0;
+      this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button1.ForeColor = System.Drawing.Color.White;
+      this.button1.Location = new System.Drawing.Point(0, 78);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(160, 52);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "Dashboard";
+      this.button1.UseVisualStyleBackColor = true;
+      // 
+      // button2
+      // 
+      this.button2.FlatAppearance.BorderSize = 0;
+      this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button2.ForeColor = System.Drawing.Color.White;
+      this.button2.Location = new System.Drawing.Point(0, 136);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(160, 52);
+      this.button2.TabIndex = 1;
+      this.button2.Text = "Register";
+      this.button2.UseVisualStyleBackColor = true;
+      // 
+      // button3
+      // 
+      this.button3.FlatAppearance.BorderSize = 0;
+      this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button3.ForeColor = System.Drawing.Color.White;
+      this.button3.Location = new System.Drawing.Point(0, 194);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(160, 52);
+      this.button3.TabIndex = 2;
+      this.button3.Text = "Employees";
+      this.button3.UseVisualStyleBackColor = true;
+      // 
+      // button4
+      // 
+      this.button4.FlatAppearance.BorderSize = 0;
+      this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button4.ForeColor = System.Drawing.Color.White;
+      this.button4.Location = new System.Drawing.Point(0, 252);
+      this.button4.Name = "button4";
+      this.button4.Size = new System.Drawing.Size(160, 52);
+      this.button4.TabIndex = 3;
+      this.button4.Text = "Statics";
+      this.button4.UseVisualStyleBackColor = true;
+      // 
+      // FormDashboard
+      // 
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+      this.ClientSize = new System.Drawing.Size(1050, 647);
+      this.Controls.Add(this.panel2);
+      this.Controls.Add(this.panel1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+      this.Name = "FormDashboard";
+      this.Text = "FormDashboard";
+      this.panel1.ResumeLayout(false);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Panel panel1;
+    private System.Windows.Forms.Button button4;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Panel panel2;
+  }
+}
